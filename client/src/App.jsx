678638a52b6bb04fe6349323a41b59af36476fb3
rohitjainfsl <rohit@fullstacklearning.com>
2024-12-22 23:19:57 +0530

    debugging errors 6

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Home from "./Pages/Home";
 import UserForm from "./registrationForm";
 import About from "./Pages/About";
@@ -8,62 +14,109 @@ import Courses from "./Pages/Courses";
 import Blog from "./Pages/Blog";
 import "./App.css";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <>
-          <Header />
-          <Navbar />
-          <Home />
-        </>
-      ),
-    },
-    {
-      path: "/registration",
-      element: (
-        <>
-          <Navbar />
-          <UserForm />
-        </>
-      ),
-    },
-    {
-      path: "/about",
-      element: (
-        <>
-          <Navbar />
-          <About />
-        </>
-      ),
-    },
-    {
-      path: "/courses",
-      element: (
-        <>
-          <Navbar />
-          <Courses />
-        </>
-      ),
-    },
-    {
-      path: "/blog",
-      element: (
-        <>
-          <Navbar />
-          <Blog />
-        </>
-      ),
-    },
-  ],
-  {
-    basename: "/",
-  }
-);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <>
+//         <Header />
+//         <Navbar />
+//         <Home />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/registration",
+//     element: (
+//       <>
+//         <Navbar />
+//         <UserForm />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/about",
+//     element: (
+//       <>
+//         <Navbar />
+//         <About />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/courses",
+//     element: (
+//       <>
+//         <Navbar />
+//         <Courses />
+//       </>
+//     ),
+//   },
+//   {
+//     path: "/blog",
+//     element: (
+//       <>
+//         <Navbar />
+//         <Blog />
+//       </>
+//     ),
+//   },
+// ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Navbar />
+              <Home />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/registration"
+          element={
+            <>
+              <Navbar />
+              <UserForm />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/about"
+          element={
+            <>
+              <Navbar />
+              <About />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/course"
+          element={
+            <>
+              <Navbar />
+              <Courses />
+            </>
+          }
+        ></Route>
+        <Route
+          path="/blog"
+          element={
+            <>
+              <Navbar />
+              <Blog />
+            </>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
