6a468fad28acba31c7d277ed92e22a5ff170643e
dheeraj1566 <dheerajjangid013@gmail.com>
2025-02-06 17:23:37 +0530

    home section work in process

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,5 +1,8 @@
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import './styles/HomeSection.css'; // Import external CSS
+// import './styles/HomeSection.css';\
+import '../styles/HomeSection.css';
+import student from '../assets/studentPicture.png'
+
 
 const Home = () => {
   document.title = "Full Stack Learning | Become a FSD the correct way";
@@ -30,9 +33,27 @@ const Home = () => {
             </div>
             </div>
           </Col>
-          <div className='second-section'>
-          <h1 className='heading-h2'>FSL- A LEARNING PLATFORM TO HELP YOU JUMP INTO TECH</h1>
-          </div>
+
+          {/* Second Section */}
+
+        <Container className="second-section-students">
+          <Col>
+            <h1 className="heading-h2">
+              FSL - A LEARNING PLATFORM TO HELP YOU JUMP INTO TECH
+            </h1>
+          </Col>
+          <Col className='parent-section-img'>
+          <Row className='student-section'>
+            <h3>CS DEGREE IS NO MORE A ROADBLOCK TO WORK IN
+            YOUR DREAM TECH BASED JOBS</h3>
+            <p>A CS degree is no longer a barrier to landing your dream tech job. Companies today prioritize skills, hands-on experience, and problem-solving abilities over formal education. With the rise of online courses, coding bootcamps, and open-source contributions, anyone with dedication and the right skill set can break into the tech industry. Many successful developers have built their careers without a traditional degree, proving that real-world experience and a strong portfolio matter more than a diploma. If you're passionate about tech, start learning, building projects, and networking—your dream job is within reach! </p>
+            <button> Explore More</button>
+          </Row>
+          <Row className='student-image'>
+            <img src={student} alt="" />
+          </Row>
+          </Col>
+        </Container>
         </Row>
       </Container>
     </section>
