1bcded11ddc4bb52b604a8e1f61bc6883c529f96
dheeraj1566 <dheerajjangid013@gmail.com>
2025-05-13 17:34:42 +0530

    create adminRegister for testing

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,5 +1,7 @@
+import adminModel from "../models/adminModel.js";
 import studentModel from "../models/studentModel.js";
-
+import authMiddleware from "../middlewares/authJWT.js";
+import bcrypt from 'bcrypt'
 
 export async function login(req, res) {
   const { email, password} = req.body;
@@ -51,12 +53,51 @@ export async function changePassword(req, res) {
 }
 
 export async function adminLogin(req, res) {
+  const { email, password } = req.body;
 
-  const {email, password} = req.bady;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required." });
+  }
 
+  try {
+    const admin = await adminModel.findOne({ email });
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
 
-  
+    const isMatch = await admin.matchPassword(password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = authMiddleware(admin._id, "admin");
+
+    res.cookie("adminToken", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 2 * 60 * 60 * 1000, 
+    });
+
+    return res.status(200).json({ message: "Admin login successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server error", error: error.message });
+  }
 }
 
+export const registerAdmin = async (req, res) => {
+  try {
+    const {email, password } = req.body;
 
-
+    let admin = await adminModel.findOne({ email });
+    if (admin) return res.status(400).json({ message: "admin already exists" });
+    console.log("password"+ password)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("hashed"+hashedPassword);
+    admin = new adminModel({email, password: hashedPassword });
+    await admin.save();
+    res.status(200).json({ message: "admin registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
