dbe1e63af6812ae47652b92d4de91091be0f6e23
rohitjainfsl <rohit@fullstacklearning.com>
2025-05-15 14:25:31 +0530

    error resolved on adminlogin

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_PATH + "",
+  baseURL: import.meta.env.VITE_BACKEND_PATH + "/api",
 });
 export default instance;
