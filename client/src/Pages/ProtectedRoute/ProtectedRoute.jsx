6ae513b65d4e9704e53f80caa1f69a99aea2bf38
rohitjainfsl <rohit@fullstacklearning.com>
2025-06-17 18:12:55 +0530

    else condition in protected route

--- client/src/Pages/ProtectedRoute/ProtectedRoute.jsx
+++ client/src/Pages/ProtectedRoute/ProtectedRoute.jsx
@@ -3,18 +3,18 @@ import { useNavigate } from "react-router-dom";
 import { adminContext } from "../Context/Admincontext";
 
 function ProtectedRoute({ children }) {
-  const { isAuthenticated, role} = useContext(adminContext);
+  const { isAuthenticated, role } = useContext(adminContext);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isAuthenticated || !(role === "admin" || role === "student")) {
-  navigate("/student/login");
-}
-
+      navigate("/student/login");
+    } else {
+      navigate("/");
+    }
   }, [isAuthenticated]);
 
   return isAuthenticated ? children : null;
 }
 
 export default ProtectedRoute;
-
